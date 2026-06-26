5584787643c617a651456fc9c35039fff3da8f7a
belikh <io@jupiter.au>
2026-06-26 20:57:50 +1000

    fix: n8n requires environment on unstable

--- modules/n8n.nix
+++ modules/n8n.nix
@@ -8,9 +8,9 @@
     enable = true;
     openFirewall = true; # Allow local mesh access if needed
 
-    settings = {
+    environment = {
       N8N_HOST = "127.0.0.1"; # Listens on localhost for Cloudflare Tunnel
-      N8N_PORT = 5678;
+      N8N_PORT = "5678";
       N8N_PROTOCOL = "http";
       WEBHOOK_URL = "https://n8n.jupiter.au";
       GENERIC_TIMEZONE = "Australia/Brisbane";
