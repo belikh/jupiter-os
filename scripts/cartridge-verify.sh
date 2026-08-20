1525dc5a8728b02e5271048ce9b403a5fdd999dd
Jupiter Belic <io@belic.net>
2026-08-20 17:31:35 +1000

    Added no-calc options to cartridge verification
    Added options to disable CRC and MD5 calculations.

--- scripts/cartridge-verify.sh
+++ scripts/cartridge-verify.sh
@@ -134,6 +134,8 @@ process_system() (
         --input "$incoming" \
         --output "$cartridge" \
         --report-output "$report" \
+        --no-calc-crc \
+        --no-calc-md5 \
         --dir-game-subdir never; then
     warn "$sys: igir exited non-zero; continuing to quarantine any unmatched staged files"
   fi
